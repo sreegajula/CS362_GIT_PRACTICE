67b5ac092ecb4f36362c6b7ee33bcad0934c4ff1
Sree Gajula <gajulas@oregonstste.edu>
2021-04-16 23:06:24 -0700

    password function

--- password.py
+++ password.py
@@ -1,2 +1,8 @@
 import string
 import random
+
+def randompass(num):
+    ascii_string = string.ascii_lowercase + string.digits + string.ascii_uppercase
+    password = ''.join(random.choice(ascii_string) for i in range(num)) ## reference from pynative
+    print(password)
+
